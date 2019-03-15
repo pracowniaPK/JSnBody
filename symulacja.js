60ed91b98aaa9e3aa7883915ea661b0c84c3847a
PKacz <kaczory5@gmail.com>
2019-03-15 19:55:23 +0100

    clean up and extract variables describing simulation

--- symulacja.js
+++ symulacja.js
@@ -1,82 +1,92 @@
+var bodyTable = [];
+var dispSize = 800;
+var wallBounce = false;
+var showcom = true;
+var initMass = 8;
+var randomMass = true;
+var initVelocity = 0;
+var randomVelocity = true;
+var bodyColor = 100;
+var comColor = 'rgba(255,0,0, 0.5)';
+var bgColor = 'rgba(255,255,255, 0.05)';
+var n = 12;
+var G = 0.1;
+
 function setup() {
-    var size = 800;
-	createCanvas(size, size);
-	fill(100);
+	createCanvas(dispSize, dispSize);
     noStroke();
 
 	class Body {
 		constructor(mass, v) {
 			this.rx = Math.random() * height;
 			this.ry = Math.random() * width;
-			this.vx = Math.random() * v;
-			this.vy = Math.random() * v;
+			this.vx = randomMass ? (Math.random() - 0.5) * v : v;
+			this.vy = randomMass ? (Math.random() - 0.5) * v : v;
 			this.ax = 0;
 			this.ay = 0;
-			this.mass = mass;
+			this.mass = randomMass ? Math.random() * mass : mass;
 		}
 	}
     
-    // generowanie n ciał
-	bt = [];
-    var i;
-    var n = 8;
-	for (i = 0; i < n; i++){
-		bt.push(new Body(Math.random() * 8, 0));
+	for (var i = 0; i < n; i++){
+		bodyTable.push(new Body(initMass, initVelocity));
 	}
 }
 
 function draw() {
-	var G = 0.1;
-    
-    // obliczanie wypadkowych sił
-	var i;
-	var j;
-	for (i = 0; i < bt.length; i++){
-		bt[i].ax = 0;
-		bt[i].ay = 0;
-		for (j = 0; j < bt.length; j++){
+	for (var i = 0; i < bodyTable.length; i++){
+		bodyTable[i].ax = 0;
+		bodyTable[i].ay = 0;
+		for (var j = 0; j < bodyTable.length; j++){
 			if (i != j) {
-				dx = bt[j].rx - bt[i].rx;
-				dy = bt[j].ry - bt[i].ry;
+				dx = bodyTable[j].rx - bodyTable[i].rx;
+				dy = bodyTable[j].ry - bodyTable[i].ry;
 				r = Math.sqrt(dx*dx + dy*dy);
 				
-                g = G * bt[i].mass * bt[j].mass / r;
-                gx = dx * g / r
-                gy = dy * g / r
-				bt[i].ax += gx / bt[i].mass;
-				bt[i].ay += gy / bt[i].mass;
+                g = G * bodyTable[i].mass * bodyTable[j].mass / r;
+                gx = dx * g / r;
+                gy = dy * g / r;
+				bodyTable[i].ax += gx / bodyTable[i].mass;
+				bodyTable[i].ay += gy / bodyTable[i].mass;
 			}
-			
         }
     }
     
-    // obliczanie przesunięć
-	for (i = 0; i < bt.length; i++){
-		bt[i].vx += bt[i].ax;
-		bt[i].vy += bt[i].ay;
-		bt[i].rx += bt[i].vx;
-		bt[i].ry += bt[i].vy;
+	for (i = 0; i < bodyTable.length; i++){
+		bodyTable[i].vx += bodyTable[i].ax;
+		bodyTable[i].vy += bodyTable[i].ay;
+		bodyTable[i].rx += bodyTable[i].vx;
+		bodyTable[i].ry += bodyTable[i].vy;
 		
-		if (bt[i].rx < 0 || bt[i].rx > height)
-			bt[i].vx = (-1) * bt[i].vx;
-		if (bt[i].ry < 0 || bt[i].ry > height)
-			bt[i].vy = (-1) * bt[i].vy;
+		if (wallBounce) {
+			if (bodyTable[i].rx < 0 || bodyTable[i].rx > height)
+				bodyTable[i].vx = (-1) * bodyTable[i].vx;
+			if (bodyTable[i].ry < 0 || bodyTable[i].ry > height)
+				bodyTable[i].vy = (-1) * bodyTable[i].vy;
+		}
 	}
-    
-    // rysowanie i liczenie środka masy
-	var mx = 0;
-    var my = 0;
-    var mm = 0;
-	for (i = 0; i < bt.length; i++){
-        fill(200 + bt[i].mass);
-		ellipse(bt[i].rx, bt[i].ry, 2 * Math.sqrt(bt[i].mass));
-		mx += bt[i].rx * bt[i].mass;
-        my += bt[i].ry * bt[i].mass;
-        mm += bt[i].mass;
+
+	for (i = 0; i < bodyTable.length; i++){
+        fill(bodyColor);
+		ellipse(bodyTable[i].rx, bodyTable[i].ry, 2 * Math.sqrt(bodyTable[i].mass));
 	}
-	fill('rgba(255,0,0, 0.5)');
-    ellipse(mx / mm, my / mm, 1);
-	fill(100);
+
+	if (showcom) {    
+		var mx = 0;
+		var my = 0;
+		var mm = 0;
+		for (i = 0; i < bodyTable.length; i++){
+			mx += bodyTable[i].rx * bodyTable[i].mass;
+			my += bodyTable[i].ry * bodyTable[i].mass;
+			mm += bodyTable[i].mass;
+		}
+		fill(comColor);
+		ellipse(mx / mm, my / mm, 1);
+	}
 	
-    background('rgba(255,255,255, 0.05)');
+	background(bgColor);
 }
+
+// function mouseClicked() {
+// 	noLoop();
+// }
